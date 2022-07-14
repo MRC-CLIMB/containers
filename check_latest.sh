8b751324e74c614bdccc80fb0f1e04f4f0d121b4
Anna Price <36504362+annacprice@users.noreply.github.com>
2022-07-14 15:14:10 +0100

    add script to check for latest software

---
+++ check_latest.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+dirlist=`ls -d */`
+#dirlist=`ls $pwd | grep /`
+IFS=$'\n'
+
+for dir in $dirlist; do
+
+    echo "$dir"
+
+    file=`ls -Art $dir | tail -n 1`
+
+    repo=`grep "about.home" $dir$file | awk -F '[/"]' '{print $6}'`
+
+    version=v`grep "software.version" $dir$file | awk -F '["]' '{print $2}'`
+
+    owner=`grep "about.home" $dir$file | awk -F '[/]' '{print $4}'`
+
+    checkgit=`grep "about.home" $dir$file | awk -F '[/]' '{print $3}'`
+
+
+    if [ $checkgit = "github.com" ]; then
+
+       latest=`curl -sL https://api.github.com/repos/$owner/$repo/releases/latest | jq -r ".tag_name"`
+
+        if [ $version != $latest ]; then
+
+           echo "$latest of $owner/$repo is available"
+
+        fi
+
+    fi
+
+done
+
